71034b46408acf740f13118ce512634ee95b7a88
calebafoster <calebalakaifoster@gmail.com>
2018-02-15 20:40:48 -0800

    klled things

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -4,21 +4,20 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
-#include <iostream>
-#include <string>
-#include <RobotDrive.h>
-#include <Joystick.h>
-#include <IterativeRobot.h>
-#include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
+#include <AnalogGyro.h>
 #include <Compressor.h>
 #include <DoubleSolenoid.h>
-#include <AnalogGyro.h>
-#include <Talon.h>
-#include <sstream>
 #include <Encoder.h>
+#include <iostream>
+#include <IterativeRobot.h>
+#include <Joystick.h>
+#include <LiveWindow/LiveWindow.h>
+#include <RobotDrive.h>
+#include <sstream>
+#include <string>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include <Talon.h>
 #include <Timer.h>
 #include "wpilib.h"
 
@@ -29,8 +28,9 @@ private:
 	frc::Joystick joystick_R {0}, joystick_L {1};
 	frc::Joystick controller {2};
 	frc::Talon intake1{6}, intake2{7},
-			   shooter1 {2}, shooter2{3},
-			   transfer1{8}, transfer2{9};
+			   shooter1 {4}, shooter2{5},
+			   transfer1{8}, transfer2{9},
+			   ledblink{9};
 	frc::Compressor *compressor = new Compressor(0);
 	frc::DoubleSolenoid ramp {0, 1}, arm {2, 3}, fire {4, 5};
 	frc::AnalogGyro gyro {1};
@@ -47,10 +47,10 @@ private:
 	 * CONTROLLER LAYOUT, DESIGNED BY PETER
 	 *
 	 * LEFT BUMPER: Intake in
-	 * RIGHT BUMPER: Intake out
+	 * RIGHT BUMPER: Intake out (and transfer to vault)
 	 *
 	 * LEFT TRIGGER: Transfer to shoooter
-	 * RIGHT TRIGGER: Transfer to vault
+	 * NOPE //RIGHT TRIGGER: Transfer to vault
 	 *
 	 * X: Arms in
 	 * Y: Arms out
@@ -59,49 +59,40 @@ private:
 	 * B: Shoot
 	 */
 
-	double shooterSpeed;
-
+	/*
+	 * BUTTON SHORTCUTS
+	 */
 	bool A() { return controller.GetRawButton(1); }
-	bool B() { return controller.GetRawButton(2); }
-	bool X() { return controller.GetRawButton(3); }
-	bool Y() { return controller.GetRawButton(4); }
-	bool Lb() { return controller.GetRawButton(5); }  // current gyro loop break
-	bool Rb() { return controller.GetRawButton(6); }
-	bool Lt() { return controller.GetRawAxis(2) > 0.5; }
-	bool Rt() { return controller.GetRawAxis(3) > 0.5; }
-	bool Lc() { return controller.GetRawButton(9); }
+	bool B() { return controller.GetRawButton(2); }  // Shooter
+	bool X() { return controller.GetRawButton(3); }  // Intake arms in
+	bool Y() { return controller.GetRawButton(4); }  // Intake arms out
+	bool Lb() { return controller.GetRawButton(5); }  // Intake in
+	bool Rb() { return controller.GetRawButton(6); }  // Intake out
+	bool Lt() { return controller.GetRawAxis(2) > 0.5; }  // Transfer cube to shooter
+	bool Rt() { return controller.GetRawAxis(3) > 0.5; }  // Nothing
+	bool Lc() { return controller.GetRawButton(9); }  // current gyro loop break
 	bool Rc() { return controller.GetRawButton(10); }
-
-	bool dpad_up() {
-		if ( (controller.GetPOV(0) >= 0 && controller.GetPOV(0) <= 45) || controller.GetPOV(0) == 315 ) {
-			return true;
-		} else {
-			return false;
-		}
+	bool dpad_up() {  // Ramp up
+		return (controller.GetPOV(0) >= 0 && controller.GetPOV(0) <= 45) /*|| controller.GetPOV(0) == 315*/;
 	}
-
-	bool dpad_down() {
-		if ( controller.GetPOV(0) >= 135 && controller.GetPOV(0) <= 215 ) {
-			return true;
-		} else {
-			return false;
-		}
+	bool dpad_down() {  // Ramp down
+		return controller.GetPOV(0) >= 135 && controller.GetPOV(0) <= 215;
 	}
+	bool dpad_left() {  // Reverse a wheel on intake
+		return controller.GetPOV(0) >= 225 && controller.GetPOV(0) <= 315;
+	}
+	bool start() { return controller.GetRawButton(8); } // Resets the gyro in teleop
+	bool R6() {return joystick_L.GetRawButton(6);}
+	double LDrive() {return joystick_L.GetY();}
+	double RDrive() {return joystick_R.GetY();}
 
-	bool start() { return controller.GetRawButton(8); } //find actual number
-
-	// Turns robot relative to active position
+	// Turns robot relative to robot's position at the time of execution.  Delays everything until done.
+	double destinationAngle, initialAngle;  bool isTurning;
 	void TurnRobot(double angle) {
-		double lastAngle = gyro.GetAngle(); //angle at which robot starts
-		double speed; //turning shooterSpeed
-		double range = 1; // Wiggle room synonym
-
-		while(fabs(angle - (gyro.GetAngle() - lastAngle)) > range && !Lc()){  //while not within range
-			speed = (angle - (gyro.GetAngle() - lastAngle)) / angle; //sets shooterSpeed based on the angle remaining
-			SmartDashboard::PutString("DB/String 2", DoubleToString(speed));
-			SmartDashboard::PutString("DB/String 0", DoubleToString(gyro.GetAngle()));
-			myRobot.TankDrive(1*speed, -1*speed);
-		}
+		gyro.Reset();
+		destinationAngle = angle;
+		isTurning = true;
+		initialAngle = gyro.GetAngle(); //angle at which robot starts
 	}
 
 	// TODO:Distance Tracking
@@ -109,22 +100,8 @@ private:
 			encoder.Reset();
 			double distLeft = dist;
 
-			int counter = 0;
-			while(encoder.GetDistance() < dist && !Lb()) {
-				std::stringstream steam;
-				std::string encoderValue;
-				steam << encoder.GetDistance();
-				steam >> encoderValue;
-				SmartDashboard::PutString("DB/String 1", encoderValue);
-
-
-				std::stringstream steam2;
-				std::string counterValue;
-				steam2 << counter;
-				steam2 >> counterValue;
-				SmartDashboard::PutString("DB/String 2", counterValue);
-
-				counter++;
+			while(encoder.GetDistance() < dist && !Lc()) {
+				SmartDashboard::PutString("DB/String 1", DoubleToString(encoder.GetDistance()));
 				distLeft = dist - encoder.GetDistance();
 				myRobot.Drive(/*distLeft < 24 ? distLeft / 96 + 0.1: 00.25*/ 0.15, 0.0);
 			}
@@ -132,9 +109,11 @@ private:
 			myRobot.Drive(0.0, 0.0);
 		}
 
-public:
+		// Set shooterSpeed to 1 so it will work without having to press up or down on the d-pad
+		double shooterSpeed = 1;
 
-	// Simple Type Converter
+public:
+	// Simple Double To String Type Converter
 	std::string DoubleToString(double i){
 		std::stringstream ss;
 		std::string s;
@@ -142,17 +121,14 @@ public:
 		ss >> s ;
 		return s;
 	}
-
 	void SetIntake(double value){
 		intake1.Set(value);
-		intake2.Set(value);
+		intake2.Set(dpad_left() ? -value : value);
 	}
-
 	void SetTransfer(double value) {
 		transfer1.Set(value);
 		transfer2.Set(value);
 	}
-
 	void SetShooter(double value) {
 		shooter1.Set(value);
 		shooter2.Set(value);
@@ -165,23 +141,36 @@ public:
 	}
 
 	void AutonomousInit() override {
-		m_autoSelected = m_chooser.GetSelected();
-		// m_autoSelected = SmartDashboard::GetString(
-		// 		"Auto Selector", kAutoNameDefault);
-		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+//		m_autoSelected = m_chooser.GetSelected(); ??
+		m_autoSelected = SmartDashboard::GetString("Auto Selector", kAutoNameDefault);
 
+		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+		SmartDashboard::PutString("DB/String 3", m_autoSelected);
 		if (m_autoSelected == kAutoNameCustom) {
 			// Custom Auto goes here
+
 		} else {
-			// Default Auto goes here
+
 		}
+
 	}
 
 	void AutonomousPeriodic() {
 		if (m_autoSelected == kAutoNameCustom) {
-			// Custom Auto goes here
+
 		} else {
-			// Default Auto goes here
+			std::string gameData;
+			gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+
+            if(gameData.length() > 0) {
+            	if(gameData[0] == 'L') {
+            		//TurnRobot(-90);
+//            		myRobot.TankDrive(-1, 1);
+            	} else {
+            		//TurnRobot(90);
+//            		myRobot.TankDrive(1, -1);
+            	}
+            }
 		}
 	}
 
@@ -193,35 +182,40 @@ public:
 
 	void TeleopPeriodic() {
 		//joystick input for the left and right drive of the robot
-		myRobot.TankDrive(joystick_L.GetY(), joystick_R.GetY());
+		myRobot.TankDrive(LDrive(), RDrive());
+
+		if (R6()) {
+			myRobot.TankDrive(-RDrive(), -LDrive());
+		}
+
+		ledblink.Set(0.65);
+
+		frc::SmartDashboard::PutString("DB/String 3", DoubleToString(joystick_R.GetY()));
 
 		/*
 		 * LEFT BUMPER: Intake In
 		 * RIGHT BUMPER: Intake Out
 		 */
 		if (Lb()) {
-			SetIntake(-1);
-		}
-		else if (Rb()) {
-			SetIntake(1);
-		}
-		else {
+			SetIntake(-0.5);
+		} else if (Rb()) {
+			SetIntake(0.5);
+		} else {
 			SetIntake(0);
 		}
 
 		/*
 		 * LEFT TRIGGER: Transfer powercube to shooter
-		 * RIGHT TRIGGER: Transfer powercube to intake/vault
+		 * RIGHT BUMPER: Transfer powercube to intake/vault
 		 */
 		if (Lt()) {
-			SetTransfer(-1);
-		}
-		else if (Rt()) {
-			SetTransfer(1);
-		}
-		else {
+			SetTransfer(-0.25);
+		} else if (Rb()) {
+			SetTransfer(0.25);
+		} else {
 			SetTransfer(0);
 		}
+
 
 		/*
 		 * DPAD UP: Ramp up
@@ -238,7 +232,7 @@ public:
 		/*
 		 *  B: Turns on shooter, then fires after one second.
 		 */
-		if (Rt()) {
+		if (B()) {
 			timer.Start();
 			SetShooter(shooterSpeed);
 			if (timer.Get() > 1)
@@ -264,14 +258,25 @@ public:
 		SmartDashboard::PutString("DB/String 0", DoubleToString(gyro.GetAngle()));
 		SmartDashboard::PutString("DB/String 1", DoubleToString(gyro.GetRate()));
 
-		//starts gyro turn test
-		if (Rc()) {
-			TurnRobot(90);
-		}
+		if (Rc())
+			isTurning=false;
 
 		//resets gyro: start button
 		if (start()){
 			gyro.Reset();
+		}
+
+		if (isTurning) {
+			double lastAngle = gyro.GetAngle(); //angle at which robot starts
+			double speed; //turning shooterSpeed
+			double range = 1; // Wiggle room
+
+			if(fabs(destinationAngle - (gyro.GetAngle() - lastAngle)) > range && !Lc()) {  //while not within range
+				speed = (destinationAngle - (gyro.GetAngle() - lastAngle)) / fabs(destinationAngle); //sets shooterSpeed based on the angle remaining
+				SmartDashboard::PutString("DB/String 2", DoubleToString(speed));
+				SmartDashboard::PutString("DB/String 0", DoubleToString(gyro.GetAngle()));
+				myRobot.TankDrive(1*speed, -1*speed);
+			}
 		}
 	}
 
